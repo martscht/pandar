0b9b2d673c9393c1bd384200b0c8d9cfb472111b
Martin Schultze <schultze@psych.uni-frankfurt.de>
2023-06-29 13:14:32 +0200

    Remove htmls via pandarize.

--- assets/rcode/pandarize.R
+++ assets/rcode/pandarize.R
@@ -4,6 +4,7 @@ pandarize <- function(x) {
   .img_location <- gsub('content', '', .location)
   .md <- gsub('.Rmd', '.md', .rmd)
   .R <- gsub('.Rmd', '.R', .rmd)
+  .html <- gsub('.Rmd', '.html', .rmd)
   
   knitr::knit(.rmd, .md, envir = new.env())
   knitr::purl(.rmd, .R, documentation = 0)
@@ -12,4 +13,6 @@ pandarize <- function(x) {
     sub(pattern = '![](', replacement = paste0('![](', .img_location), x = _, fixed = TRUE) |>
     sub(pattern = '<img src="figure/', replacement = paste0('<img src="', .img_location, '/', x , '_files/figure-html/')) |>
     writeLines(con = .md)
+  
+  file.remove(.html)
 }
