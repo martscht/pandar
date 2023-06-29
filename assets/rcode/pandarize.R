a6d786593f5812815be1a660a12080b4a3d8b73d
Martin Schultze <schultze@psych.uni-frankfurt.de>
2023-06-29 13:11:29 +0200

    Fix more typos in pandarize.

--- assets/rcode/pandarize.R
+++ assets/rcode/pandarize.R
@@ -1,9 +1,9 @@
 pandarize <- function(x) {
   .rmd <- dir(getwd(), pattern = paste0(x, '.Rmd'), recursive = TRUE)
-  .location <- gsub(paste0(x, '.Rmd'), '', rmd)
-  .img_location <- gsub('content', '', location)
-  .md <- gsub('.Rmd', '.md', rmd)
-  .R <- gsub('.Rmd', '.R', rmd)
+  .location <- gsub(paste0(x, '.Rmd'), '', .rmd)
+  .img_location <- gsub('content', '', .location)
+  .md <- gsub('.Rmd', '.md', .rmd)
+  .R <- gsub('.Rmd', '.R', .rmd)
   
   knitr::knit(.rmd, .md, envir = new.env())
   knitr::purl(.rmd, .R, documentation = 0)
