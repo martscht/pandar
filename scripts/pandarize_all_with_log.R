4ba080c24217209c55867785d4253b6d1faa8622
Kevin Pommeranz <s6753790@stud.uni-frankfurt.de>
2025-01-29 14:48:20 +0100

    Script to pandarize all files in a folder (currently on all of content)

---
+++ scripts/pandarize_all_with_log.R
@@ -0,0 +1,39 @@
+# Required library
+library(tools)
+
+apply_function_to_rmds <- function(base_folder) {
+  # Get the path of the script's directory
+  script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)  # For RStudio
+  
+  # Set the log file to be saved in the script's directory
+  log_file <- file.path(script_dir, "process_log.txt")
+  
+  # Start saving the console output to a file
+  sink(log_file, append = TRUE)  # `append = TRUE` ensures logs are added to the file
+  
+  cat("Starting the processing...\n")
+  
+  # List all .Rmd files in the base folder and subdirectories
+  rmd_files <- list.files(base_folder, pattern = "\\.Rmd$", full.names = TRUE, recursive = TRUE)
+  
+  for (file in rmd_files) {
+    # Extract the filename without the extension (without ".Rmd")
+    rmd_name <- tools::file_path_sans_ext(basename(file))
+    
+    # Check if a corresponding .R file exists to match prior Purl choice
+    r_file <- file.path(dirname(file), paste0(rmd_name, ".R"))
+    r_file_exists <- file.exists(r_file)
+    
+    # Call pandarize with filename and Purl boolean
+    pandarize(rmd_name, r_file_exists)
+  }
+  
+  cat("Processing complete.\n")
+  
+  # Stop saving the console output to the file
+  sink()
+}
+
+# Run the function with your base folder path and log file
+base_folder <- "content"
+apply_function_to_rmds(base_folder)
